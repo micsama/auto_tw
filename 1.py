58b260c6f529156a176ba9769fd6bee5c6d98f77
Xing Ji <dzmfg@JinoMacBook-Pro.local>
2021-12-02 20:54:05 +0800

    11

--- 1.py
+++ 1.py
@@ -21,61 +21,62 @@ def fillcode(page):
         sleep(0.2)
         page.keyboard.press(code1[i])
     # page.fill("//html/body/div[2]/div[1]/div[2]/div/div[1]/div/div/form[1]/div[4]/input", code1)
-def run(playwright: Playwright, name, passwd) -> None:
-    if platform == "linux":
-        browser = playwright.chromium.launch(headless=True)
-    elif platform == "darwin":
-        browser = playwright.chromium.launch(channel="msedge", headless=False)
-    elif platform == "win32":
-        browser = playwright.chromium.launch(channel="msedge", headless=True)
-    else:
-        print("系统支持有误，请检查")
-        exit(1)
-    context = browser.new_context()
-    page = context.new_page()
-    page.set_default_timeout(60000)
-    try:
-        global Alldata
-        page.goto(Alldata["url"])
-        sleep(0.5)
-        page.fill("[placeholder=\"账号 Username\"]", name)
-        page.fill("[placeholder=\"密码 Password\"]", passwd)
-        fillcode(page)
-        page.click("text=登 录")
-        sleep(1.5)
-        if page.is_visible("text=验证码信息无效。"):
-            logging.info("验证码识别出错")
-            return False
-        if page.is_visible("text=账户不存在。"):
-            logging.info("账户名有误")
-            return False
-        if page.is_visible("text=登 录"):
-            logging.info("登录失败，可能是密码有误")
-            return False
-        c = randint(0, 5)
-        page.wait_for_timeout(6000)
-        #获取用户名
-        name_path = '//html/body/div/div/div/div/div[1]/div[2]/div/form/div[6]/div[1]/div/div[2]/div/div/span'
-        uname = page.text_content(name_path)
-        if uname == "":
-            logging.info("获取姓名失败，可能是网络问题")
-        page.fill("input[name=\"tw\"]", "36." + str(c))
-
-        logging.info(name + uname + "开始填报，体温" + "36." + str(c))
-        page.click("button:has-text(\"提交\")")
-        sleep(0.5)
-        if page.is_visible("text=健康填报成功"):
-            page.click("text=确定")
-            logging.info(name + "成功！")
-            context.close()
-            browser.close()
-            return True
+def run( name, passwd) -> None:
+    with sync_playwright() as playwright:
+        if platform == "linux":
+            browser = playwright.chromium.launch(headless=True)
+        elif platform == "darwin":
+            browser = playwright.chromium.launch(channel="msedge", headless=False)
+        elif platform == "win32":
+            browser = playwright.chromium.launch(channel="msedge", headless=True)
         else:
-            logging.info('填报网站时出错')
+            print("系统支持有误，请检查")
+            exit(1)
+        context = browser.new_context()
+        page = context.new_page()
+        page.set_default_timeout(60000)
+        try:
+            global Alldata
+            page.goto(Alldata["url"])
+            sleep(0.5)
+            page.fill("[placeholder=\"账号 Username\"]", name)
+            page.fill("[placeholder=\"密码 Password\"]", passwd)
+            fillcode(page)
+            page.click("text=登 录")
+            sleep(1.5)
+            if page.is_visible("text=验证码信息无效。"):
+                logging.info("验证码识别出错")
+                return False
+            if page.is_visible("text=账户不存在。"):
+                logging.info("账户名有误")
+                return False
+            if page.is_visible("text=登 录"):
+                logging.info("登录失败，可能是密码有误")
+                return False
+            c = randint(0, 5)
+            page.wait_for_timeout(6000)
+            #获取用户名
+            name_path = '//html/body/div/div/div/div/div[1]/div[2]/div/form/div[6]/div[1]/div/div[2]/div/div/span'
+            uname = page.text_content(name_path)
+            if uname == "":
+                logging.info("获取姓名失败，可能是网络问题")
+            page.fill("input[name=\"tw\"]", "36." + str(c))
+
+            logging.info(name + uname + "开始填报，体温" + "36." + str(c))
+            page.click("button:has-text(\"提交\")")
+            sleep(0.5)
+            if page.is_visible("text=健康填报成功"):
+                page.click("text=确定")
+                logging.info(name + "成功！")
+                context.close()
+                browser.close()
+                return True
+            else:
+                logging.info('填报网站时出错')
+                return False
+        except BaseException as e:
+            logging.info(e)
             return False
-    except BaseException as e:
-        logging.info(e)
-        return False
 
 
 def loaddata():
@@ -103,21 +104,20 @@ def func(data):
     if errorflag>=5:
         exit(1)
     i = 1
-    with sync_playwright() as playwright:
-        for j  in range(len(data)):
-            user = data[j]
-            wait_time = randint(0, 300)
-            print(f"正在处理第{i}个，总共{len(data)}个。并等待{wait_time}秒后提交下一个")
-            i = i + 1
-            logging.info(user)
-            if run(playwright, user["name"], user["passwd"]):
-                if len(argv)>1:
-                    sleep(wait_time)
-                del data[j]
-            else:
-                logging.info(user["name"] + "填报失败，半分钟后重试")
-                errorflag+=1
-                func(data)
+    for j  in range(len(data)):
+        user = data[j]
+        wait_time = randint(0, 300)
+        print(f"正在处理第{i}个，总共{len(data)}个。并等待{wait_time}秒后提交下一个")
+        i = i + 1
+        logging.info(user)
+        if run(playwright, user["name"], user["passwd"]):
+            if len(argv)>1:
+                sleep(wait_time)
+            del data[j]
+        else:
+            logging.info(user["name"] + "填报失败，半分钟后重试")
+            errorflag+=1
+            func(data)
     return 0
     pass
 
