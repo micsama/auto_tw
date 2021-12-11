c191a98c17ea02a030332816522df081f590653f
Ji Xing <dzmfg@JideMacBook-Pro.local>
2021-12-11 18:36:33 +0800

    xiugai

--- 1.py
+++ 1.py
@@ -116,7 +116,7 @@ def loaddata():
 
 
 def report(qq):
-    qqurl = "https://qmsg.zendee.cn/send/" + Alldata['key'] + "?msg=体温填报失败！请手动填报qq=" + qq
+    qqurl = "https://qmsg.zendee.cn/send/" + Alldata['token'] + "?msg=体温填报失败！请手动填报qq=" + qq
     res = requests.get(qqurl)
     pass
 
